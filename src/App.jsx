a42da054729ef0117147b18592492d5e1917154b
Damilare Gabriel Idowu <dammy@damilareidowu.com>
2023-09-12 16:40:05 +0100

    Bypass Auth

--- src/App.jsx
+++ src/App.jsx
@@ -189,7 +189,7 @@ export default function App() {
 
             // Miscs
           }}>
-          {authenticated ? (
+          {!authenticated ? (
             <Layout>
               <Toast options={toastOptions} />
             </Layout>
